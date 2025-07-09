4ad52cfc85c5a285d238000108e533f1f94e9904
DaniExp <dani00exp@gmail.com>
2025-07-09 18:12:45 +0200

    Implement basic BEP-20 requirements (structure)

---
+++ RaffleToken42.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2025 Daniel Expósito dani00exp@gmail.com
+pragma solidity ^0.8.3;
+
+contract Raffle42 {
+    uint256 supply = 1000000;
+
+    function totalSupply() external view returns (uint256){
+        return supply;
+    }
+    function balanceOf(address _account) external view returns(uint256) {
+
+    }
+    function transfer(address _to, uint256 _value) public {
+
+    }
+    function allowance() public view returns(uint256) {
+
+    }
+    function approve(uint256 _amount) external {
+
+    }
+    function transferFrom(address _from, address _to, uint256 _value) public {
+
+    }
+
+}
