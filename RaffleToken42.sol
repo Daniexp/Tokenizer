caac3dda835757d078af8e0679e3db1378252849
DaniExp <dani00exp@gmail.com>
2025-07-10 18:04:35 +0200

    Add ERC20 library and basic raffle struct

--- RaffleToken42.sol
+++ RaffleToken42.sol
@@ -1,27 +1,21 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2025 Daniel Expósito dani00exp@gmail.com
-pragma solidity ^0.8.3;
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Raffle42 {
-    uint256 supply = 1000000;
+struct Raffle {
+	address winner;
+	uint256 firstIndex;
+	uint256 totalTickets;
+	mapping(address => uint256[2]) player; 
+}
 
-    function totalSupply() external view returns (uint256){
-        return supply;
-    }
-    function balanceOf(address _account) external view returns(uint256) {
+contract Raffle42 is ERC20 {
+	uint256 supply = 1000000;
+	Raffle[] raffle;
 
-    }
-    function transfer(address _to, uint256 _value) public {
 
-    }
-    function allowance() public view returns(uint256) {
-
-    }
-    function approve(uint256 _amount) external {
-
-    }
-    function transferFrom(address _from, address _to, uint256 _value) public {
-
-    }
-
+	constructor() ERC20("RAFFLE42", "RAF42") {
+		_mint(msg.sender, supply);
+	}
 }
