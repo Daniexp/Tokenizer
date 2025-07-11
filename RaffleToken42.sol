d8119e9b1de9a21d904f454b58f7753c6a4d6848
DaniExp <dani00exp@gmail.com>
2025-07-11 14:05:06 +0200

    basic buytickets and finishraffle methods, pending testing

--- RaffleToken42.sol
+++ RaffleToken42.sol
@@ -1,21 +1,46 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2025 Daniel Expósito dani00exp@gmail.com
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 struct Raffle {
 	address winner;
-	uint256 firstIndex;
-	uint256 totalTickets;
-	mapping(address => uint256[2]) player; 
+	address[] tickets; 
 }
 
 contract Raffle42 is ERC20 {
-	uint256 supply = 1000000;
 	Raffle[] raffle;
+    address  internal owner;
 
 
 	constructor() ERC20("RAFFLE42", "RAF42") {
-		_mint(msg.sender, supply);
-	}
+		_mint(msg.sender, 1000000);
+        owner = msg.sender;
+    }
+    function getActualRaffle() internal view returns(uint256) {
+        require(raffle.length > 0);
+        return raffle.length - 1;
+    }
+    // BuyTickets - require to have x amount of tokens and pay them as tickets
+    function buytickets(uint256 _amount) public {
+        require(_amount > 0, "Insuficient funds");
+        require(raffle.length > 0, "Actually no raffle is started");
+        uint256 raffleIndex = getActualRaffle();
+        for (uint16 i = 0; i < _amount; i++) 
+        {
+            raffle[raffleIndex].tickets.push(msg.sender);
+        }
+    }
+    // Create Raffle
+    // Set raffle winner
+    // Finish Raffle - set winner, close raffle and create new raffle
+    function finishRaffle() public returns(address) {
+        require(raffle.length > 0, "Actually no raffle is started");
+        require(msg.sender == owner, "Only the owner can finish a raffle");
+        uint raffleIndex = getActualRaffle();
+        require(raffle[raffleIndex].tickets.length > 0, "The raffle doesn't have participants");
+        uint256 winner =  uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, raffleIndex))) % (raffle[raffleIndex].tickets.length);
+        raffle[raffleIndex].winner = raffle[raffleIndex].tickets[winner];
+        return raffle[raffleIndex].winner;
+    }
 }
