621f6cbf0eda6b8c05cde780a72a5e3aabd1b5af
DaniExp <dani00exp@gmail.com>
2025-07-16 19:37:05 +0200

    modify raffles array to map, raffles with raffleId, timestmap, maxtickets and deployTimeStamp

--- RaffleToken42.sol
+++ RaffleToken42.sol
@@ -5,45 +5,60 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 struct Raffle {
 	address winner;
-	address[] tickets; 
+    uint256 maxTickets;
+	address[] tickets;
+    uint256 daysDuration;
+    string name;
+    uint256 deployTimestamp;
+    uint256 endTimestamp;
+    // Prize will be a nft that ideally will represent the ownership of some item in the real world or a digital asset
+}
+struct Person {
+    string name;
+    address wallet;
 }
 
 contract Raffle42 is ERC20 {
-	Raffle[] internal raffle;
+    mapping (uint256 => Raffle) public raffles;
     address  internal owner;
 
 
 	constructor() ERC20("RAFFLE42", "RAF42") {
 		owner = msg.sender;
-        raffle.push(Raffle({winner: address(0), tickets: new address[](0)}));
-
         _mint(msg.sender, 1000000);
     }
-    function getActualRaffle() internal view returns(uint256) {
-        require(raffle.length > 0);
-        return raffle.length - 1;
+
+    // Create Raffle
+    // Returns raffleId
+    function createRaffle(string memory _name, uint256 _maxTickets, uint256 _daysDuration) public returns (uint256) {
+        require(msg.sender == owner, "Only the owner can create a raffle");
+        Raffle memory newRaffle;
+        newRaffle.name = _name;
+        newRaffle.maxTickets = _maxTickets;
+        newRaffle.daysDuration = _daysDuration;
+        newRaffle.deployTimestamp = block.timestamp;
+        uint256 newRaffleId = uint256(keccak256(abi.encodePacked(_name, block.timestamp)));
+        raffles[newRaffleId] = newRaffle;
+        return newRaffleId;
     }
+
     // BuyTickets - require to have x amount of tokens and pay them as tickets
-    function buytickets(uint256 _amount) public {
-        require(_amount > 0, "Insuficient funds");
-        require(raffle.length > 0, "Actually no raffle is started");
-        uint256 raffleIndex = getActualRaffle();
-        for (uint16 i = 0; i < _amount; i++) 
+    function buytickets(uint256 _amount, uint256 _raffleId) public {
+        require(_amount > 0, "You can't buy 0 tickets");
+        require(msg.sender != owner, "The owner can't buy tickets");
+        for (uint256 i = 0; i < _amount; i++)
         {
-            raffle[raffleIndex].tickets.push(msg.sender);
+            raffles[_raffleId].tickets.push(msg.sender);
         }
     }
     // Create Raffle
     // Set raffle winner
     // Finish Raffle - set winner, close raffle and create new raffle
-    function finishRaffle() public returns(address) {
-        require(raffle.length > 0, "Actually no raffle is started");
+    function finishRaffle(uint256 _raffleId) public returns(address) {
         require(msg.sender == owner, "Only the owner can finish a raffle");
-        uint raffleIndex = getActualRaffle();
-        require(raffle[raffleIndex].tickets.length > 0, "The raffle doesn't have participants");
-        uint256 winner =  uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, raffleIndex))) % (raffle[raffleIndex].tickets.length);
-        raffle[raffleIndex].winner = raffle[raffleIndex].tickets[winner];
-        raffle.push(Raffle({ winner: address(0), tickets: new address[](0)}));
-        return raffle[raffleIndex].winner;
+        require(raffles[_raffleId].tickets.length > 0, "The raffle doesn't have participants");
+        uint256 winner =  uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, _raffleId))) % (raffles[_raffleId].tickets.length);
+        raffles[_raffleId].winner = raffles[_raffleId].tickets[winner];
+        return raffles[_raffleId].winner;
     }
 }
